7032dd052d8dcd03c946e9d2c82a9d86349ef16b
Anthony Scott <ascott329@gmail.com>
2016-09-29 11:41:08 -0500

    add-oligo component

---
+++ src/app/add-oligo/add-oligo.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { AddOligoComponent } from './add-oligo.component';
+
+describe('Component: AddOligo', () => {
+  it('should create an instance', () => {
+    let component = new AddOligoComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/add-oligo/add-oligo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-oligo',
+  templateUrl: './add-oligo.component.html',
+  styleUrls: ['./add-oligo.component.css']
+})
+export class AddOligoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
