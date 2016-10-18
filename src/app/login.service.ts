190f9d753483d18066f96c9a78156182f8aba51b
Anthony Scott <ascott329@gmail.com>
2016-10-18 13:24:00 -0500

    switch to heroku

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -12,7 +12,7 @@ export class LoginService{
 
   private token: string;
   private headers = new Headers({'Content-Type': 'application/json'});
-  private login_url = 'https://www.canthonyscott.com:1107/api/api-token-auth/';
+  private login_url = 'https://injcalcapi.herokuapp.com/api-token-auth/';
 
   constructor(private http: Http) { }
 
